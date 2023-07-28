5485810e8512222180ed0a1a21c858fcde205889
WVALDEZGALI <camiloga17310@gmail.com>
2023-07-28 20:37:36 -0300

    Clase2

---
+++ B_Variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero = 20;
+        String nombre = "Camilo";
+        char letra = 'l';
+        double temperatura = 38.5;
+        boolean siONo = false;
+        long largo = 562452458;
+        
+    }
+}
