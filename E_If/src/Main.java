55e5eafab1e5544499b2f7253a0c371f4b60baff
WVALDEZGALI <camiloga17310@gmail.com>
2023-07-31 19:41:05 -0300

    Clase4

---
+++ E_If/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 8;
+
+        if (num1>num2) {
+
+            System.out.println("El numero 1 es mayor que el numero 2");
+
+        }
+        else {
+
+            System.out.println("El numero 1 es menor que el numero 2");
+
+        }
+        
+    }
+}
