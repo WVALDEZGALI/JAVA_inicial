7405ff55f6a8850ead53caf3a12d6cd0c06c7ce2
WVALDEZGALI <camiloga17310@gmail.com>
2023-08-06 13:43:26 -0300

    Clase8

--- K_Ejercicio_5_repetitivas_condicionales/src/Main.java
+++ K_Ejercicio_5_repetitivas_condicionales/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Ingrese su DNI");
+        System.out.println("Ingrese numero de DNI");
         Scanner dniIng = new Scanner(System.in);
         int dni = dniIng.nextInt();
 
@@ -30,21 +30,32 @@ public class Main {
         Scanner edadIng = new Scanner(System.in);
         int edad = edadIng.nextInt();
 
-        while ( dni != 0 && ! nombre.equalsIgnoreCase ("fin") ){
+        while (  dni != 0 && !nombre.equalsIgnoreCase("fin") ){
+
+            System.out.println("El dni es: "+ dni + " El nombre es: "+ nombre + " y la edad es: "+ edad);
 
             if (edad >= 6 && edad <= 10) {
-                System.out.println("Ingrese su Edad");
+                System.out.println("La categoria correspondiente es: Menores 1 (de 6 a 10 años)");
             } else if (edad >= 11 && edad <= 17) {
-
+                System.out.println("La categoria correspondiente es: Menores 2 (de 11 a 17 años)");
             } else if (edad >= 18 && edad <= 30) {
-
+                System.out.println("La categoria correspondiente es: Juvenil (de 18 a 30 años)");
             } else if (edad >= 31 && edad <= 50) {
-
-            } else if (edad < 50) {
-
+                System.out.println("La categoria correspondiente es: Adultos (de 31 a 50 años)");
+            } else if (edad > 50) {
+                System.out.println("La categoria correspondiente es: Adultos mayores (mayores de 50 años)");
             } else {
-
+                System.out.println("No cumples con la edad minima para poder participar");
             }
+
+            System.out.println("Ingrese su DNI");
+            dni = dniIng.nextInt();
+
+            System.out.println("Ingrese su Nombre");
+            nombre = nombreIng.nextLine();
+
+            System.out.println("Ingrese su Edad");
+            edad = edadIng.nextInt();
 
         }
 
