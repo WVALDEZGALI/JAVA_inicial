71c31c2d05c120bb2876746026fa352f96b90e55
WVALDEZGALI <camiloga17310@gmail.com>
2023-08-07 11:40:40 -0300

    "Clase9"

---
+++ Ejercicio_6_arborl/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //declarar la altura del arbol
+        int altura = 10;
+
+        //bucle para recorrer las filas del arbol
+        for (int fila = 0; fila < altura; fila ++) {
+
+            //bucle para los espacios
+            for (int espacio = 0; espacio <(altura - fila - 1); espacio ++) {
+                System.out.print(" ");
+            }
+            //bucle para los asteriscos
+            for (int asterisco = 0; asterisco <(fila * 2)+1; asterisco ++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+
+        }
+
+        //tronco del arbol
+        int largoTronco = 2;
+        for (int base = 0; base<largoTronco; base ++) {
+
+            //espacios en blanco
+            for (int espacioBlanco = 0; espacioBlanco <(altura - 2); espacioBlanco ++) {
+                System.out.print(" ");
+            }
+            //barras del tronco
+            for (int barras = 0; barras < 3; barras ++) {
+                System.out.print("|");
+            }
+
+            System.out.println("");
+
+        }
+    }
+}
