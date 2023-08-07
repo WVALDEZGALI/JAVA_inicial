8a4de6b60adc4ce9411239df8a6e4f1aedd721e7
WVALDEZGALI <camiloga17310@gmail.com>
2023-08-07 20:26:37 -0300

    Clase 10

---
+++ M_Arrays_vectores/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    // Declaracion
+        int vector [] = new  int [4];
+
+    // Asignacion (manual)
+        vector[0]=2;
+        vector[1]=12;
+        vector[2]=22;
+        vector[3]=48;
+
+        System.out.println(vector[1]);
+
+
+    }
+}
