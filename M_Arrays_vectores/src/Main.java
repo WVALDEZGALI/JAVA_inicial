14fe2cc5a16ab054258832710495c89f36eedab7
WVALDEZGALI <camiloga17310@gmail.com>
2023-08-08 13:33:06 -0300

    Clase 10

--- M_Arrays_vectores/src/Main.java
+++ M_Arrays_vectores/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
 
@@ -7,13 +8,28 @@ public class Main {
         int vector [] = new  int [4];
 
     // Asignacion (manual)
-        vector[0]=2;
+        /*vector[0]=2;
         vector[1]=12;
         vector[2]=22;
-        vector[3]=48;
+        vector[3]=48;*/
 
-        System.out.println(vector[1]);
+        Scanner teclado = new Scanner(System.in);
 
+        for ( int i = 0; i < vector.length; i ++ ){
+
+            System.out.println("Ingrese el valor para el indice " + i);
+            vector[i] = teclado.nextInt();
+        }
+
+    // Recorrido
+
+        for ( int i = 0; i < vector.length; i ++ ){
+
+            System.out.println("Estoy en el indice: " + i);
+            System.out.println("Tengo guardado un " + vector[i]);
+            System.out.println("---------------------");
+
+        }
 
     }
 }
