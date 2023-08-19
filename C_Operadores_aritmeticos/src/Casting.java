db63dfa9a3956557c9c403a33e16e6f81d1f5c6d
WVALDEZGALI <camiloga17310@gmail.com>
2023-08-18 21:53:30 -0300

    Platzi

---
+++ C_Operadores_aritmeticos/src/Casting.java
@@ -0,0 +1,31 @@
+public class Casting {
+
+    public static void main(String[] args) {
+
+        double d = 86.45;
+        int i = (int) d; // Cast de el numero dicimal, estimacion, es dicir se elimina la parte dicimal
+        System.out.println (i);
+
+        double dogs = 30.0 / 12.0;
+        System.out.println (dogs);
+
+        int estimatedDogs = (int) dogs; //Estimacion.
+        System.out.println (estimatedDogs);
+
+        int a = 30;
+        int b = 12;
+        System.out.println (a / b);
+        System.out.println ((double) a / b); //Exactitud.
+
+        double c = a / b;
+        System.out.println (c); //Cast automatico.
+
+        char n = '1';
+        int nI = n;
+        System.out.println (nI); //Cast a entero. 49 equivale a '1' en la tabla ASCII.
+
+        short nS = (short) n;
+        System.out.println (nS);
+
+    }
+}
---
+++ C_Operadores_aritmeticos/src/MathematicOperations.java
@@ -0,0 +1,21 @@
+public class MathematicOperations {
+    public static void main(String[] args) {
+
+        double x = 2.1;
+        double y = 3.5;
+
+        System.out.println (Math.ceil (x)); // Devuelve un entero hacia arriba.
+        System.out.println (Math.floor (y)); // Devuelve un entero hacia abajo.
+        System.out.println (Math.pow (x,y)); // Devuelve un numero elevado a otro, primero es el numero y el segundo el exponenete.
+        System.out.println (Math.max (x,y)); // Devuelve el numero mayor.
+        System.out.println (Math.sqrt (y)); // Devuelve la raiz cuadrada.
+        System.out.println (Math.PI * Math.pow (y,2)); // Devuelve el area de un circulo, donde "y" es el radio de circunferencia.
+        System.out.println (4 * Math.PI * Math.pow (x,2)); // Devuelve el area de una esfera, donde "x" es el radio de circunferencia.
+        System.out.println ((4/3) * Math.PI * Math.pow (x,3)); // Devuelve el volumen de una esfera, donde "x" es el radio de circunferencia..
+
+
+
+
+
+    }
+}
